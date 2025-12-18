a8a4612926f884e4b7464aa0e9f26ca5753f61ec
Anthony <anthonnhani@gmail.com>
2025-12-18 13:05:06 -0700

    Set up the hero page and project structure

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { watchFile } = require("fs");
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: "development",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true
+    },
+    devtool: "eval-source-map",
+
+    devServer: {
+        watchFiles: "./src/template.html",
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        }),
+
+    ],
+
+    module : {
+        rules: [
+            {
+                test: "/\.css$/i", // Regex matching css file types
+                use:  ["style-loader", "css-loader"] // Loaders applied to css imports
+            },
+            {
+                test: "/\.(png|jpg|jpeg)$/i",
+                use: "asset/ressource",
+            }
+        ]
+    }
+}
